2450bb5648af504357714e6bbf351ba4586acf9e
Matt1998 <tsekinping2010@gmail.com>
2022-11-19 21:46:55 +0800

    Add pages

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    return render_template('/index.html')
+
+
+@app.route('/charts')
+def charts():
+    return render_template('/charts.html')
 
 
 if __name__ == '__main__':
